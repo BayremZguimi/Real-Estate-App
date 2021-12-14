f929242e15d3a2b9e0061096162425ef8bff98d9
BayremZguimi <mohamedbayrem.zguimi1@esprit.tn>
2021-12-14 17:31:48 +0100

    final

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -4,13 +4,13 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-import { baseurl, fetchApi } from '../../utils/fetchApi';
-import ImageSrollbar from '../../components/ImageScrollbar';
 
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
+import ImageScrollbar from '../../components/ImageScrollbar';
 
 const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
   <Box maxWidth='1000px' margin='auto' p='4'>
-    {photos && <ImageSrollbar data={photos} />}
+    {photos && <ImageScrollbar data={photos} />}
     <Box w='full' p='6'>
       <Flex paddingTop='2' alignItems='center'>
         <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
@@ -62,7 +62,7 @@ const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title
 export default PropertyDetails;
 
 export async function getServerSideProps({ params: { id } }) {
-  const data = await fetchApi(`${baseurl}/properties/detail?externalID=${id}`);
+  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
   
   return {
     props: {
